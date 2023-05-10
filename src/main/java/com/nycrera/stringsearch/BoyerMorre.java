33bd947f608f3f351ec602ac5fbfa8a3513d9da4
Alperen Yurdakul <alperenyurdakul7@gmail.com>
2023-05-10 12:14:33 +0300

    Somewhat better BoyerMoore / still not all correct

--- src/main/java/com/nycrera/stringsearch/BoyerMorre.java
+++ src/main/java/com/nycrera/stringsearch/BoyerMorre.java
@@ -1,6 +1,7 @@
 package com.nycrera.stringsearch;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -11,7 +12,7 @@ public class BoyerMorre {
 	private List<Integer> Pattern;
 	private List<Integer> Matches = new ArrayList<>();
 	private HashMap<Integer, Integer> badCharacter = new HashMap<>();
-	private HashMap<Integer, Integer> goodPrefix = new HashMap<>();
+	private HashMap<Integer, Integer> goodSuffix = new HashMap<>();
 
 	public int getComparisonCount() {
 		return comparisonCount;
@@ -41,9 +42,88 @@ public class BoyerMorre {
 
 		badCharacter.put(0, patternLength);
 	}
-	
-	private void createGoodPrefixTable(){
+
+	private void createGoodPrefixTable() {
+		for (int i=0;i<Pattern.size()-1;i++)
+		{
+			List<Integer> suffix = Pattern.subList(Pattern.size()-1-i,Pattern.size());
+			List<Integer> rest = Pattern.subList(0,Pattern.size()-1-i); 
+			
+			int d = findSuffix(suffix, rest);
+			
+			if (d == -1)
+				d = Pattern.size();
+			
+			
+			goodSuffix.put(suffix.size(), d);
+		}
 		
+		
+		for (Integer k : goodSuffix.keySet()) {
+		    if (goodSuffix.get(k) == Pattern.size()) {
+		        int d = findPrefix(k);
+		        goodSuffix.put(k, d);
+		    }
+		}
+	}
+	
+	private int findSuffix(List<Integer> suffix, List<Integer> rest) {
+				if (rest.size() < suffix.size())
+					return -1;
+				
+				int i=rest.size()-1;
+				int j=suffix.size()-1;
+				boolean match = false;
+				
+				while (j>-1 && i>-1 && !match)
+				{
+					do
+					{
+						if (suffix.get(j)==rest.get(i))
+						{
+							match = true;
+							j--;
+							if (j>-1) // fix negative index
+								i--;
+						}
+						else
+						{
+							match = false;
+							j=suffix.size()-1;
+						}
+						
+						if (j>i)
+							return -1;
+						
+					}while (j>-1 && match);
+					// check if the found suffix is preceded by the same character value
+					if (i > 0 && match && rest.get(rest.size()-1) == rest.get(i-1))
+					{
+						// then, keep searching to start of the string
+						match = false;
+						j=suffix.size()-1;
+					}
+					
+					if (match)	
+						return Pattern.size()-1 -( i+suffix.size()-1);
+					
+					i--;
+				}
+				
+				return -1;
+	}
+	
+	private int findPrefix(int k) {
+		for (int i=k-1; i>0; i--)
+		{
+			List<Integer> prefix = Pattern.subList(0,i);
+			List<Integer> suffix = Pattern.subList(Pattern.size()-i,Pattern.size());
+			
+			if (prefix.equals(suffix) && goodSuffix.get(suffix.size()) != Pattern.size())
+				return Pattern.size()-i;
+		}
+		
+		return Pattern.size();
 	}
 
 	public void Run() {
@@ -54,7 +134,7 @@ public class BoyerMorre {
 		while (i < stringLength) {
 			int j = patternLength - 1;
 
-			while (j >= 0) {
+			while (j >= 0) { // keep looking until we find a mismatch or a full match
 				comparisonCount++;
 				if (Pattern.get(j) == File.get(i - patternLength + 1 + j)) { // Character match
 					j--;
@@ -67,9 +147,15 @@ public class BoyerMorre {
 				Matches.add(i - patternLength + 1); // Match detected
 				i++;
 			} else {
-				Integer badCharacterShift = badCharacter.getOrDefault(Pattern.get(patternLength - 1), patternLength);
-				Integer goodPrefixShift = goodPrefix.getOrDefault(j, -1);
-				i += Math.max(badCharacterShift, i - (i - patternLength + 1 + j) - goodPrefixShift);
+				// d1 = Max(badCharacterHueristic - successfulmatchcount, 1)
+				Integer badCharacterShift = Math.max(badCharacter.getOrDefault(Pattern.get(patternLength - 1), patternLength) - j, 1);
+				
+				Integer goodPrefixShift = goodSuffix.getOrDefault(j, -1);
+				if(j == 0) { // if no match, use d1 as the pattern shift
+					i+= badCharacterShift; 
+				}else { // else use max(d1,d2)
+					i += Math.max(badCharacterShift, goodPrefixShift);
+				}
 			}
 		}
 	}
@@ -88,10 +174,13 @@ public class BoyerMorre {
 		}
 		System.out.println("-----------------------");
 	}
-	
+
 	public void printGoodPrefix() {
 		System.out.println("--Good Prefix Table--");
-		
+		for (Map.Entry<Integer, Integer> entry : badCharacter.entrySet()) {
+			int shiftAmount = entry.getValue();
+			System.out.println("d("+entry.getKey()+") : " + shiftAmount);
+		}
 		System.out.println("----------------------");
 	}
 }
